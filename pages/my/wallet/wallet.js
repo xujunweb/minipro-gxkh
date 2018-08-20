519160df9c16b175625dc01fa65bc6cab62ae314
xujun <xujun@youanmi.com>
2018-08-20 18:01:55 +0800

    余额页面，充值押金

--- pages/my/wallet/wallet.js
+++ pages/my/wallet/wallet.js
@@ -2,8 +2,9 @@
 const app = getApp()
 Page({
   data: {
-    userInfo: {},
-    hasUserInfo: false,
+    balance:'80.5',       //余额
+    deposit:false,      //是否缴纳押金
+    disabled:true       //提现开启
   },
   onLoad: function () {
   },
