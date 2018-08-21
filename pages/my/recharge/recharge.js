f46b9caf261970bdb19629235b141ce820146eaa
xj <xu5621818@126.com>
2018-08-21 23:20:01 +0800

    页面添加

--- pages/my/recharge/recharge.js
+++ pages/my/recharge/recharge.js
@@ -5,14 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    moneyList:[
+      {
+        money:5,
+        select:true
+      },
+      {
+        money: 10,
+        select: false
+      },
+      {
+        money: 20,
+        select: false
+      },
+      {
+        money: 30,
+        select: false
+      },
+      {
+        money: 50,
+        select: false
+      },
+      {
+        money: 100,
+        select: false
+      }
+    ],
+    selectMoney:5
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
@@ -28,39 +54,16 @@ Page({
   onShow: function () {
   
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  //选择金额
+  select:function(e){
+    var index = e.target.dataset.index
+    for (var i = 0, len = this.data.moneyList.length;i<len;i++){
+      this.data.moneyList[i].select = false
+    }
+    this.data.moneyList[index].select = true
+    this.data.selectMoney = this.data.moneyList[index].money
+    this.setData({
+      moneyList: this.data.moneyList
+    })
   }
 })
