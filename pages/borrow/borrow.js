da94629cc99631f2d049eb9af41f62ac22b26d96
xujun <xujun@youanmi.com>
2018-11-21 19:32:59 +0800

    xxx

--- pages/borrow/borrow.js
+++ pages/borrow/borrow.js
@@ -79,7 +79,7 @@ Page({
           //余额不足，跳转余额充值页面
           wx.showModal({
             title: '提示',
-            content: '您有订单尚未结算完成，请前往充值',
+            content: `您好！您有订单${Math.abs(userInfo.money)}元暂未结清，请前往充值结清并留意余额帐户，支付本次使用金额。`,
             success: (res) => {
               if (res.confirm) {
                 wx.navigateTo({
