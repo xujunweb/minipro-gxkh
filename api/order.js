06248eb016bcd7bf3590d67869f0b5308d92d139
xujun <xujun@youanmi.com>
2019-04-24 16:55:09 +0800

    xxx

--- api/order.js
+++ api/order.js
@@ -1,10 +1,10 @@
 import ajax from '../utils/ajax.js'
-
 //获取代理商订单列表
 export const getOrderList = (data) => {
+  var app = getApp()
   return ajax({
     url: wx.envConfig.host + 'lockOrder/pageByLockOrder',
-    data: { ...data },
+    data: { ...data, agency_user_id: app.globalData.loginUserInfo.id },
     method: 'post',
     loading:true,
   })
@@ -12,9 +12,10 @@ export const getOrderList = (data) => {
 
 //获取代理商设备列表
 export const getDeviceList = (data) => {
+  var app = getApp()
   return ajax({
     url: wx.envConfig.host + 'lockInfo/pageByLockInfo',
-    data: { ...data },
+    data: { ...data, user_id: app.globalData.loginUserInfo.id },
     method: 'post',
     loading: true,
   })
